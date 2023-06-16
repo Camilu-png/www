53795141b0ebe84c7fb3a3f93000147da151e7f7
Camila Arancibia <camila.arancibiaf@sansano.usm.cl>
2023-06-15 21:17:51 -0400

    feat(index): add graphql endpoint

--- server/index.js
+++ server/index.js
@@ -1,28 +1,37 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import connectDB from './mongodb/connect.js';
-dotenv.config();
+import connectDB from "./mongodb/connect.js";
+
+import { graphqlHTTP } from "express-graphql";
+import { schema, root } from "./graphql/schema.js";
+
+dotenv.config({ path: "../.env" });
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send({message: 'Hello World!'});
-})
-
-
-const startServer = async() => {
-    try{
-        //connect to the database...
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
-    
-    } catch(error){
-        console.log(error);
-    }
-}
+const startServer = async () => {
+  try {
+    connectDB(
+      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DBNAME}`
+    );
+    app.listen(8080, () =>
+      console.log("Server started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
