bb165849742eda0ebb88c35d0fda1e9e5966390b
Camila Arancibia <camila.arancibiaf@sansano.usm.cl>
2023-07-02 19:13:23 -0400

    fix(.): update login

---
+++ server/controllers/User.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const { Router } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = Router();
+
+User.create({
+  name: "admin",
+  password: "admin",
+  email: "admin@email.com",
+  type: "admin",
+});
+
+User.create({
+  name: "zimi",
+  password: "zimi",
+  email: "zimi@email.com",
+  type: "doctor",
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    res.json(await User.create(req.body));
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.findOne({ username: req.body.username });
+    if (user) {
+      const result = req.body.password === user.password;
+      if (result) {
+        const token = await jwt.sign({ username: user.username }, SECRET);
+        res.json({ token });
+      } else {
+        res.status(400).json({ error: "password doesn't match" });
+      }
+    } else {
+      res.status(400).json({ error: "User doesn't exist" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+module.exports = router;
